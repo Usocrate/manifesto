34fdf456a78d6fbe0db1e23c24000b7421afd22b
florent <florent@usocrate.fr>
2017-04-12 12:09:02 +0200

    Mise en ordre structure de la page d'accueil (draft)

--- index.php
+++ index.php
@@ -13,8 +13,8 @@
 </head>
 <body>
 	<div class="container">
-		<div class="row">
-			<div class="col-lg-12">
+		<section id="usocrateDef">
+			<header>
 				<div class="jumbotron">
 					<div class="card">
 						<h1>Usocrate.fr</h1>
@@ -29,62 +29,49 @@
 						</dl>
 					</div>
 				</div>
-			</div>
-		</div>
-		<div class"row">
-			<div class="col-lg-12">
-				<h2>Manifeste</h2>
-				<h3>Pourquoi ?</h3>
-				<p>
-					Le numérique est le moteur d’une révolution sociale qui va bien au-delà des problématiques technologiques.<br>
+			</header>
+		</section>
+		<section id="manifesto">
+			<h2>Manifeste<br><small>Pour une usocratie numérique</small></h2>
+			<p>
+					Le numérique est le moteur d’une révolution sociale qui ne peut se réduire à des problématiques technologiques.<br>
 					Sa pratique transforme en profondeur les relations interpersonnelles, les rapports de production ou encore la manière dont individuellement nous nous représentons le monde.<br>
 					Les anciens modes de production industriels, ne permettent plus de construire des outils numériques à la hauteur des enjeux collectifs et individuels liés à la transformation actuelle.
-				</p>
+			</p>
+
+			<h3>1 conviction</h3>
+			<div>
 				<p>
-					Ce manifeste propose une méthode : replacer les usagers au centre des attentions dans le processus de conception de produit / service numérique.<br>
+					Replacer les usagers au centre des attentions dans le processus de conception de produit / service numérique.<br>
 					Il fait de la valeur d’usage le premier critère de mesure du progrès et de l’innovation.<br>
 					Il s’oppose à une vision de l’innovation faite de gadgets technologiques consommés sur un mode ludique et hautement périssables.<br>
 				</p>
-				<p>
-					L’ambition est humaniste : celle d’améliorer la vie des gens, notamment dans leurs conditions de travail, en concevant des outils numériques adaptés, efficaces, libérant les potentiels individuels.<br>
-					L’ambition est éthique : celle de mettre le numérique au service de l'intelligence humaine plutôt que d’en faire un vecteur d’aliénation et de déshumanisation.
-				</p>
-				<p>
-					La compétitivité de notre industrie numérique est aussi au premier plan.<br>
-					Être usocrate, c’est être dépité par l’absence de leaders français du numérique massivement adoptés à l'échelle mondiale ou simplement nationale; absence dûe en partie à une apathie généralisée face à la nécessité de faire de bon produits pour pouvoir être compétitifs.
-				</p>
-				<p>
-					Donner une place centrale aux usages dans la conception de produit ou service c’est sécuriser les investissements en assurant la viabilité des services numériques développés qu’ils le soient dans un esprit de service public ou pour le secteur marchand.<br>
-					Se focaliser sur les usages est le meilleur vecteur d’innovation pour les entreprises en ne les condamnant pas à copier leurs concurrents à moindre coût pour pouvoir générer du chiffre d’affaires.
-				</p>
-				<p>
-					Pour ceux qui portent traditionnellement une démarche de conception centrée sur l’humain et les usages (ergonomes, designers), trop d’énergie est encore dépensée à convaincre les donneurs d’ordre et à obtenir les moyens de travailler.
-				</p>
-				<p>
-					Ce manifeste veut contribuer à lever certains archaïsmes trop courants dans la conception de produits / services numériques pour libérer l’innovation et augmenter l’impact et l’utilité sociale du numérique.<br>
-					Il se veut un moyen, parmi d’autres, pour les professionnels du numérique de partager cette vision.
-				</p>
-				<p>#design de service, #design thinking, #expérience, #usage, #empowerment</p>
-				<h3>Pour qui ?</h3>
-				<p>
-					L’ensemble des professionnels du secteur numérique qui ont entre les mains la destinée de services numériques : chef de produit, designers, chef de projets, financeurs.
-					(Prendre le créneau In-house product design là où l’Expérience utilisateur comme jadis le packaging est une affaire de gens du marketing.)
-				</p>
-				<p>
-					Pour les signataires une adhésion consiste à :
+			</div>
+			<div class="row">
+				<div class="col-md-6">
+					<h4>Humanisme et éthique</h4>
 					<ul>
-						<li>Partager la vision du métier, donner du poids à un discours commun</li>
-						<li>Raviver la conscience de sa responsabilité individuelle (éthique -> impact sur la vie des gens) et s’engager :<br>
-						la mise en oeuvre de ce programme ne peut se faire sans transformation des organisations et des modes de production traditionnels; il s’agit de s’engager à transformer ces organisations
-						mise en oeuvre dans pratiques professionnelles quotidiennes; en témoigner
-						promotion de la démarche
-						</li>
+						<li>Améliorer la vie des gens, notamment dans leurs conditions de travail, en concevant des outils numériques adaptés, efficaces, libérant les potentiels individuels.</li>
+						<li>Mettre le numérique au service de l'intelligence humaine plutôt que d’en faire un vecteur d’aliénation et de déshumanisation.</li>
 					</ul>
-				</p>
-				<h3>Principes</h3>
-				<div class="row">
+				</div>
+				<div class="col-md-6">
+					<h4>Efficacité économique et innovation</h4>
+					<p>
+						La compétitivité de notre industrie numérique est aussi au premier plan.<br>
+						Être usocrate, c’est être dépité par l’absence de leaders français du numérique massivement adoptés à l'échelle mondiale ou simplement nationale; absence dûe en partie à une apathie généralisée face à la nécessité de faire de bon produits pour pouvoir être compétitifs.
+					</p>
+					<p>
+						Donner une place centrale aux usages dans la conception de produit ou service c’est sécuriser les investissements en assurant la viabilité des services numériques développés qu’ils le soient dans un esprit de service public ou pour le secteur marchand.<br>
+						Se focaliser sur les usages est le meilleur vecteur d’innovation pour les entreprises en ne les condamnant pas à copier leurs concurrents à moindre coût pour pouvoir générer du chiffre d’affaires.
+					</p>
+				</div>
+			</div>			
+
+			<h3>3 engagements</h3>
+			<div class="row">
 					<div class="col-md-4">
-						<h4>Porter toute l'attention sur les usagers</h4>
+						<h4>1- Porter toute l'attention aux usagers</h4>
 						<ul>
 							<li>Aller à la rencontre des usagers : pas de conception sans usagers</li>
 							<li>Respecter les usagers</li>
@@ -92,14 +79,14 @@
 						</ul>
 					</div>
 					<div class="col-md-4">
-						<h4>Contribuer à faire évoluer les indicateurs de réussite d’un projet</h4>
+						<h4>2- Changer les indicateurs de réussite d’un projet</h4>
 						<ul>
 							<li>La valeur d’usage comme projet industriel</li>
 							<li>Mesurer la réussite : appropriation, satisfaction, feedback</li>
 						</ul>
 					</div>
 					<div class="col-md-4">
-						<h4>Réaffirmer la production numérique et l’innovation comme une pratique sociale.</h4>
+						<h4>3- Réaffirmer la production de valeur comme une pratique sociale.</h4>
 						<ul>
 							<li>Collaborer pour l’efficacité, dépasser les conflits d’intérêts</li>
 							<li>Collaborer pour la créativité</li>
@@ -108,66 +95,95 @@
 						</ul>
 					</div>
 			</div>
-		</div>
-		<div class="row">
-			<div class="col-lg-4">
-				<blockquote class="twitter-tweet" lang="fr">
+
+			<h3>A qui s'adresse ce manifeste ?</h3>
+			<div>
 					<p>
-						Il n&#39;y aura pas de <a href="https://twitter.com/hashtag/num%C3%A9rique?src=hash">#numérique</a> qui vaille sans <a href="https://twitter.com/hashtag/design?src=hash">#design</a> : qualité d&#39;usage, appropriation, séduction aussi. Ne pas pousser l&#39;un sans l&#39;autre
+						L’ensemble des professionnels du secteur numérique qui ont entre les mains la destinée de services numériques : chef de produit, designers, chef de projets, financeurs.
+						(Prendre le créneau In-house product design là où l’Expérience utilisateur comme jadis le packaging est une affaire de gens du marketing.)
 					</p>
-					&mdash; Alain Cadix (@AlainCadix) <a href="https://twitter.com/AlainCadix/status/504523149359480832">27 Août 2014</a>
-				</blockquote>
-				<blockquote class="twitter-tweet" lang="fr">
 					<p>
-						Important design things : <a href="http://t.co/DVwlOYppjO">pic.twitter.com/DVwlOYppjO</a>
+						Pour ceux qui portent traditionnellement une démarche de conception centrée sur l’humain et les usages (ergonomes, designers), trop d’énergie est encore dépensée à convaincre les donneurs d’ordre et à obtenir les moyens de travailler.
 					</p>
-					&mdash; DesignAndHuman (@designandhuman) <a href="https://twitter.com/designandhuman/status/492423920050798592">24 Juillet 2014</a>
-				</blockquote>
-				<blockquote class="twitter-tweet" lang="fr">
+					<ul>
+						<li>Il veut contribuer à lever certains archaïsmes trop courants dans la conception de produits / services numériques pour libérer l’innovation et augmenter l’impact et l’utilité sociale du numérique.</li>
+						<li>Il se veut un moyen, parmi d’autres, pour les professionnels du numérique de partager cette vision.</li>
+					</ul>
 					<p>
-						Rechercher l’intérêt de l’utilisateur et le reste suivra&#10;<a href="https://t.co/uNFpthLicE">https://t.co/uNFpthLicE</a>
+						Pour les signataires une adhésion consiste à :
+						<ul>
+							<li>Partager la vision du métier, donner du poids à un discours commun</li>
+							<li>Raviver la conscience de sa responsabilité individuelle (éthique -> impact sur la vie des gens) et s’engager :<br>
+							la mise en oeuvre de ce programme ne peut se faire sans transformation des organisations et des modes de production traditionnels; il s’agit de s’engager à transformer ces organisations
+							mise en oeuvre dans pratiques professionnelles quotidiennes; en témoigner
+							promotion de la démarche
+							</li>
+						</ul>
 					</p>
-					&mdash; Florent Chanavat (@Usocrate) <a href="https://twitter.com/Usocrate/status/492011900033855488">23 Juillet 2014</a>
-				</blockquote>
 			</div>
-			<div class="col-lg-4">
-				<blockquote class="twitter-tweet" lang="fr">
-					<p>
-						Design is creativity with a goal. <a href="http://t.co/Gn4zyeqOHU">pic.twitter.com/Gn4zyeqOHU</a>
-					</p>
-					&mdash; Cooper (@cooper) <a href="https://twitter.com/cooper/status/467408695983607808">16 Mai 2014</a>
-				</blockquote>
-				<blockquote class="twitter-tweet" lang="fr">
-					<p>
-						Vision systémique, extraordinaire conviction et obsession de la simplicité.&#10;<a href="http://t.co/o2HYFOIZuA">http://t.co/o2HYFOIZuA</a>
-					</p>
-					&mdash; Florent Chanavat (@Usocrate) <a href="https://twitter.com/Usocrate/status/512661070310154241">18 Septembre 2014</a>
-				</blockquote>
+		</section>
+
+		<section id="tweets">
+			<div class="row">
+				<div class="col-lg-4">
+					<blockquote class="twitter-tweet" lang="fr">
+						<p>
+							Il n&#39;y aura pas de <a href="https://twitter.com/hashtag/num%C3%A9rique?src=hash">#numérique</a> qui vaille sans <a href="https://twitter.com/hashtag/design?src=hash">#design</a> : qualité d&#39;usage, appropriation, séduction aussi. Ne pas pousser l&#39;un sans l&#39;autre
+						</p>
+						&mdash; Alain Cadix (@AlainCadix) <a href="https://twitter.com/AlainCadix/status/504523149359480832">27 Août 2014</a>
+					</blockquote>
+					<blockquote class="twitter-tweet" lang="fr">
+						<p>
+							Important design things : <a href="http://t.co/DVwlOYppjO">pic.twitter.com/DVwlOYppjO</a>
+						</p>
+						&mdash; DesignAndHuman (@designandhuman) <a href="https://twitter.com/designandhuman/status/492423920050798592">24 Juillet 2014</a>
+					</blockquote>
+					<blockquote class="twitter-tweet" lang="fr">
+						<p>
+							Rechercher l’intérêt de l’utilisateur et le reste suivra&#10;<a href="https://t.co/uNFpthLicE">https://t.co/uNFpthLicE</a>
+						</p>
+						&mdash; Florent Chanavat (@Usocrate) <a href="https://twitter.com/Usocrate/status/492011900033855488">23 Juillet 2014</a>
+					</blockquote>
+				</div>
+				<div class="col-lg-4">
+					<blockquote class="twitter-tweet" lang="fr">
+						<p>
+							Design is creativity with a goal. <a href="http://t.co/Gn4zyeqOHU">pic.twitter.com/Gn4zyeqOHU</a>
+						</p>
+						&mdash; Cooper (@cooper) <a href="https://twitter.com/cooper/status/467408695983607808">16 Mai 2014</a>
+					</blockquote>
+					<blockquote class="twitter-tweet" lang="fr">
+						<p>
+							Vision systémique, extraordinaire conviction et obsession de la simplicité.&#10;<a href="http://t.co/o2HYFOIZuA">http://t.co/o2HYFOIZuA</a>
+						</p>
+						&mdash; Florent Chanavat (@Usocrate) <a href="https://twitter.com/Usocrate/status/512661070310154241">18 Septembre 2014</a>
+					</blockquote>
+				</div>
+				<div class="col-lg-4">
+					<blockquote class="twitter-tweet" lang="fr">
+						<p>Great programmers delete code.&#10;Great interface designers delete UI.</p>
+						&mdash; Luke Wroblewski (@lukew) <a href="https://twitter.com/lukew/status/501777829973880832">19 Août 2014</a>
+					</blockquote>
+					<blockquote class="twitter-tweet" lang="fr">
+						<p>
+							Engineers don&#39;t let engineers design user interfaces. <a href="http://t.co/XKSDUOxKHe">pic.twitter.com/XKSDUOxKHe</a>
+						</p>
+						&mdash; John Bellomy (@cowbs) <a href="https://twitter.com/cowbs/status/516045565847535616">28 Septembre 2014</a>
+					</blockquote>
+					<blockquote class="twitter-tweet" lang="fr">
+						<p>Optional features are what you look for when you&#39;re about to buy, and quickly forget about the moment you own.</p>
+						&mdash; John Maeda (@johnmaeda) <a href="https://twitter.com/johnmaeda/status/584901037187825664">6 Avril 2015</a>
+					</blockquote>
+					<blockquote class="twitter-tweet" lang="fr">
+						<p>
+							Target ratios of UX designers to developers: Minimum 1:12, ideally 1:4. Recommendations from <a href="https://twitter.com/leahbuley">@leahbuley</a>’s research.
+						</p>
+						&mdash; Jesse James Garrett (@jjg) <a href="https://twitter.com/jjg/status/565613568314572801">11 Février 2015</a>
+					</blockquote>
+					<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+				</div>
 			</div>
-			<div class="col-lg-4">
-				<blockquote class="twitter-tweet" lang="fr">
-					<p>Great programmers delete code.&#10;Great interface designers delete UI.</p>
-					&mdash; Luke Wroblewski (@lukew) <a href="https://twitter.com/lukew/status/501777829973880832">19 Août 2014</a>
-				</blockquote>
-				<blockquote class="twitter-tweet" lang="fr">
-					<p>
-						Engineers don&#39;t let engineers design user interfaces. <a href="http://t.co/XKSDUOxKHe">pic.twitter.com/XKSDUOxKHe</a>
-					</p>
-					&mdash; John Bellomy (@cowbs) <a href="https://twitter.com/cowbs/status/516045565847535616">28 Septembre 2014</a>
-				</blockquote>
-				<blockquote class="twitter-tweet" lang="fr">
-					<p>Optional features are what you look for when you&#39;re about to buy, and quickly forget about the moment you own.</p>
-					&mdash; John Maeda (@johnmaeda) <a href="https://twitter.com/johnmaeda/status/584901037187825664">6 Avril 2015</a>
-				</blockquote>
-				<blockquote class="twitter-tweet" lang="fr">
-					<p>
-						Target ratios of UX designers to developers: Minimum 1:12, ideally 1:4. Recommendations from <a href="https://twitter.com/leahbuley">@leahbuley</a>’s research.
-					</p>
-					&mdash; Jesse James Garrett (@jjg) <a href="https://twitter.com/jjg/status/565613568314572801">11 Février 2015</a>
-				</blockquote>
-				<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
-			</div>
-		</div>
+		</section>
 	</div>
 	<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
 	<?php if (defined('GA_KEY') && defined('GA_ACCOUNT') ) : ?>
