ca48076e325726c71fa740e3c939bbb4d4c85cb9
Florent Chanavat <florent@usocrate.fr>
2017-04-29 09:39:30 +0000

    Focus sur fichier manifesto (nettoyage)

--- cv/inc/foot.inc.php
+++
@@ -1,7 +0,0 @@
-<a href="http://www.usocrate.fr">Usocrate 2000-2009</a>
-<!--
-<a href="http://validator.w3.org/check?uri=referer"><img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0!" height="31" width="88" /></a>&nbsp;
-<a href="http://my.opera.com/Florent/affiliate/" title="Download Opera" >
-<img src="http://promote.opera.com/small/opera80x15.gif" width="80" height="15" alt="Opera" />
-</a>
--->
