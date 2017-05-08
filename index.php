8fcee5169e3d805b8c2240af70cf5e6b74adbb98
Florent Chanavat <florent@usocrate.fr>
2017-05-08 11:17:32 +0000

    Contenu

--- index.php
+++ index.php
@@ -124,7 +124,7 @@
 				<div class="card-group">
 					<div class="card">
 						<div class="card-block">
-							<h5 class="card-title">Aller à la rencontre des usagers : pas de conception sans usagers</h5>
+							<h5 class="card-title">Aller à la rencontre des usagers</h5>
 							<div class="card-text">
 								<p>Moi usocrate...</p>
 								<ul>
@@ -152,11 +152,12 @@
 								<p>Moi usocrate...</p>
 								<ul>
 									<li>Je ne discrédite pas les usagers et ne justifient pas des dysfonctionnements par leur incapacité</li>
+									<li>Je valorise l'écoute</li>
 									<li>J’admets l’idée que les comportements des usagers avec le produit ou service numérique soit en décalage avec ce qui était attendu :  je suis capable d’intégrer le principe de réalité</li>
 									<li>Je prends soin des usagers (éthique, économie de l’attention, ...)</li>
 									<li>Je ne m’approprie pas le service, ne me substitue pas aux usagers</li>
 									<li>Je cadre l'expression des goûts personnels de l'équipe projet.</li>
-									<li>Affirmer la dimension éthique du design</li>
+									<li>Je me soucie de l'impact des choix de conception sur la vie des usagers et j'affirmer la dimension éthique du design</li>
 								</ul>
 							</div>
 						</div>
@@ -175,35 +176,42 @@
 						</div>
 					</div>
 				</div>
-
+				
+				<h4>2- Changer les indicateurs de réussite</h4>
 				<div class="card-group">
 					<div class="card">
 						<div class="card-block">
-							<h4 class="card-title">2- Changer les indicateurs de réussite d’un projet</h4>
+							<h5 class="card-title">La valeur d’usage comme projet industriel</h5>
 							<div class="card-text">
-								<ul>
-									<li>La valeur d’usage comme projet industriel<br>Sortir du modèle de la société de consommation. De la vision marketing : vendre une production abondante, fétichisme de l’objet, différenciation statutaire sans fin (Baudrillard). Pour un développement rationnel et durable.</li>
-									<li>Mesurer la réussite : appropriation, satisfaction, feedback</li>
-								</ul>
+								<p>Sortir du modèle de la société de consommation.<br>De la vision marketing : vendre une production abondante, fétichisme de l’objet, différenciation statutaire sans fin (Baudrillard). Pour un développement rationnel et durable.</p>
 								<div><span class="badge badge-default">charge cognitive</span>, <span class="badge badge-default">expérience utilisateur</span></div>
 								<div>
 									<p>Moi usocrate...</p>
 									<ul>
-											<li>Je ne considère pas que le travail soit terminé lorsqu'une fonctionnalité attendue est disponible. Il faut qu'elle soit localisée et utilisée par les usagers.</li>
-											<li>Je ne considère pas que le travail soit fait lorsque les données attendues sont diffusées. Il faut qu'elles soient consultables et portent une valeur métier </li>
-											<li>Je refuse que la faisabilité technique ou la disponibilité des développeurs informatiques décide du devenir du produit / service</li>
-											<li>Je valorise l'écoute</li>
-											<li>Une de mes qualités est la réactivité</li>
-											<li>Je fais de la simplicité du produit / service un objectif<br>Stopper la déviance fonctionnelle - pas d'arrêt du dev de fonctionnalités c'est de la valeur ajoutée, mais pas d'exposition intégrale du fonctionnel : génératrice de complexité, de coût de dev et de maintenance. Des vues glanceable : halte à l'interactivité. unbundling (profil tâches). Pas de développement de nouvelles fonctionnalités sans organisation, intégration et valorisation  (utilité, utilisabilité)<br>Nécessaires à l'attractivité du service le marketing et l'habillage visuel de l'application, il faut plus pour garantir durablement l'engagement des usagers</li>
-											<li>Je ne confonds pas nouveauté et innovation.</li>
-											<li>Je crois que c'est la valeur d'usage qui permet d'asseoir la pérennité d'un produit / service</li>
-											<li>Je considère que ce n’est ni la somme des fonctionnalités, ni le temps consacré à développer ces fonctionnalités qui fait la valeur du produit; ce n'est pas parce que c'est possible techniquement et à moindre coût que ça apporte de la valeur au produit.</li>
-											<li>Je pense le choix du framework et des outils permettant dev le produit et service est négligeable dans la valeur aux usagers.</li>
-											<li>Je crois que la démonstration technologique à toujours rendu davantage service aux forces de ventes en déficit d'argument qu'aux usagers.</li>
-											<li>Je pense que l'argument de non faisabilité technique n'est en 2017 plus recevable sans en étudier les coûts.</li>
-											<li>Je pense que l'échec de beaucoup de produit / service numériques est prévisible lorsque l'utilité et la désirabilité de ceux n'a pas été questionnées auprès des usagers</li>
-											<li>Je pense qu'il y a dans les projets numériques beaucoup de temps perdu à des activités qui n'ont pas d'impact sur la qalité du produit / service livré aux usagers</li>
+										<li>Je ne considère pas que le travail soit terminé lorsqu'une fonctionnalité attendue est disponible. Il faut qu'elle soit localisée et utilisée par les usagers.</li>
+										<li>Je ne considère pas que le travail soit fait lorsque les données attendues sont diffusées. Il faut qu'elles soient consultables et portent une valeur métier </li>
+										<li>Je refuse que la faisabilité technique ou la disponibilité des développeurs informatiques décide du devenir du produit / service</li>
+										<li>Je fais de la simplicité du produit / service un objectif<br>Stopper la déviance fonctionnelle - pas d'arrêt du dev de fonctionnalités c'est de la valeur ajoutée, mais pas d'exposition intégrale du fonctionnel : génératrice de complexité, de coût de dev et de maintenance. Des vues glanceable : halte à l'interactivité. unbundling (profil tâches). Pas de développement de nouvelles fonctionnalités sans organisation, intégration et valorisation  (utilité, utilisabilité)<br>Nécessaires à l'attractivité du service le marketing et l'habillage visuel de l'application, il faut plus pour garantir durablement l'engagement des usagers</li>
+										<li>Je ne confonds pas nouveauté et innovation.</li>
+										<li>Je crois que c'est la valeur d'usage qui permet d'asseoir la pérennité d'un produit / service</li>
+										<li>Je considère que ce n’est ni la somme des fonctionnalités, ni le temps consacré à développer ces fonctionnalités qui fait la valeur du produit; ce n'est pas parce que c'est possible techniquement et à moindre coût que ça apporte de la valeur au produit.</li>
+										<li>Je pense le choix du framework et des outils permettant dev le produit et service est négligeable dans la valeur aux usagers.</li>
+										<li>Je crois que la démonstration technologique à toujours rendu davantage service aux forces de ventes en déficit d'argument qu'aux usagers.</li>
+										<li>Je pense que l'argument de non faisabilité technique n'est en 2017 plus recevable sans en étudier les coûts.</li>
+										<li>Je pense que l'échec de beaucoup de produit / service numériques est prévisible lorsque l'utilité et la désirabilité de ceux n'a pas été questionnées auprès des usagers</li>
+										<li>Je pense qu'il y a dans les projets numériques beaucoup de temps perdu à des activités qui n'ont pas d'impact sur la qualité du produit / service livré aux usagers</li>
 									</ul>
+								</div>								
+							</div>
+						</div>
+					</div>
+					<div class="card">
+						<div class="card-block">
+							<h5 class="card-title">Mesurer les résultats</h5>
+							<div class="card-text">
+								<p>Mesurer la réussite : appropriation, satisfaction, feedback</p>
+								<p>Design with a purpose</p>
+								<div>
 									<p>Moi usocrate...</p>
 									<ul>
 										<li>émotion (confiance), effort, productivité</li>
@@ -221,7 +229,7 @@
 											<li>unbundling (profil tâches)</li>
 											<li>pour se mettre en mouvement (segmentation fonctionnelle) le design c'est le passage entre l'industrialisable (API) et le consommable</li>
 											<li>Nouveauté, rénovation, adhésion tendances</li>
-											<li>à copier les autres ont perd de la valeur</li>
+											<li>A copier les autres ont perd de la valeur</li>
 											<li>Ventes, valeur d’échange</li>
 											<li>pas de garanties moyen termes si absence d’innovation dans un domaine fortement concurrentiel</li>
 											<li>Le taux d’occupation des ressources (développeurs) n’est pas un critère</li>
@@ -229,18 +237,21 @@
 										</ul>
 										<li>Mise en place systématique d’une démarche et d’outils de captation des retours d’expérience</li>
 									</ul>
-								</div>
+								</div>								
 							</div>
 						</div>
-					</div>
+					</div>					
+				</div>
+				
+				<h4 class="card-title">3- Réaffirmer la production de valeur comme une pratique sociale.</h4>
+				<div class="card-group">
 					<div class="card">
 						<div class="card-block">
-							<h4 class="card-title">3- Réaffirmer la production de valeur comme une pratique sociale.</h4>
 							<div class="card-text">
 								<ul>
 									<li>Collaborer pour l’efficacité, dépasser les conflits d’intérêts</li>
 									<li>Itérer pour converger autour d'une vision commune</li>
-									<li>Collaborer pour la créativité</li>
+									<li>Collaborer pour la créativité, valoriser l'expérimentation</li>
 									<li>Faire la promotion de la culture design</li>
 									<li>Le design est un processus : pour la planification</li>
 									<li>Halte au gaspillage : toute l'énergie consacrée à la création de valeur</li>
@@ -251,6 +262,7 @@
 									<li>Je m’assure que tous les participants au projet se sentent légitimes (Empowerment)</li>
 									<li>Je pense que l'appropriation des enjeux et des objectifs par l'ensemble des membre de l'équipe projet est un facteur clé de réussite</li>
 									<li>Je vise l'harmonie et la résolution des conflits plutôt que leur exploitation à des fins politiques</li>
+									<li>Une de mes qualités est la réactivité</li>
 									<li>Je travaille à casser les silos</li>
 									<li>Je pense que certains livrables sont inutiles et privilégie l'échange à la documentation</li>
 									<li>Je tente de cadrer l'expression des goûts personnels de l'équipe projet.</li>
@@ -270,24 +282,24 @@
 	          <div class="col-xs-12 col-md-8">
 							<div class="card">
 								<div class="card-block">
-			            <h3 class="text-center card-title">Je suis Usocrate !</h3>
+			            			<h3 class="text-center card-title">Je suis Usocrate !</h3>
 									<div class="card-text">
 										<p>Peut-on vous compter parmi les rangs des usocrates ?</p>
 										<ul>
 											<li>Je partage la vision, je souhaite donner du poids à ce discours</li>
 											<li>J'ai conscience de ma responsabilité individuelle et je témoigne de mon engagement à mettre en oeuvre les principes du manifeste dans mes pratiques professionnelles quotidiennes</li>
 										</ul>
-				            <form method="POST" action="">
-				                  <div class="form-group ">
-														<label>Je me présente</label>
-														<input class="form-control input-lg" id="" name="url" value="" placeholder="un profil web Twitter, LinkedIn,..." type="url">
-				                  </div>
-													<div class="form-group ">
-														<label>Un email ?</label>
-														<input required="" class="form-control input-lg" id="" name="email" value="" placeholder="pour être informé de l'évolution de l'initiative usocrate.fr" type="email">
-				                  </div>
-				                  <button type="submit" class="btn btn-block btn-primary">Oui, j'en suis</button>
-				            </form>
+							            <form method="POST" action="">
+							                  <div class="form-group ">
+												<label>Je me présente</label>
+												<input class="form-control input-lg" id="" name="url" value="" placeholder="un profil web Twitter, LinkedIn,..." type="url">
+							                  </div>
+												<div class="form-group ">
+												<label>Un email ?</label>
+												<input required="" class="form-control input-lg" id="" name="email" value="" placeholder="pour être informé de l'évolution de l'initiative usocrate.fr" type="email">
+							                  </div>
+							                  <button type="submit" class="btn btn-block btn-primary">Oui, j'en suis</button>
+							            </form>
 									</div>
 								</div>
 							</div>
