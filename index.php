a5e35577828aca6650abfe15c06dc557f8602dc4
Florent Chanavat <florent@usocrate.fr>
2018-10-29 09:17:15 +0000

    Correction grossière erreur grammaticale

--- index.php
+++ index.php
@@ -38,12 +38,11 @@ header('charset=utf-8');
 		<div class="jumbotron"><h1>Manifeste <span class="badge badge-info" style="display:none">beta</span><br><small>Pour une usocratie numérique</small></h1></div>
 		<main>
 			<section id="intro">
-				<div class="goahead"><i class="fas fa-angle-down"></i><i class="fas fa-angle-down"></i><i class="fas fa-angle-down"></i></div>
 				<p>
 					Le numérique est le moteur d'une révolution sociale qui dépasse les problématiques technologiques.<br>
 					Sa pratique transforme en profondeur les relations interpersonnelles, les conditions de travail et la manière dont nous nous représentons le monde.</p>
 				<p>
-					Si les usages du numérique sont largement étudiés et commentés, les conditions dans lesquelles sont construites ces applications qui occupent nos vies est plus rarement abordée.<br>
+					Si les usages du numérique sont largement étudiés et commentés, les conditions dans lesquelles sont construites ces applications qui occupent nos vies sont plus rarement abordées.<br>
 					C'est pourtant là que tout se décide.<br>
 					Et le constat est simple : les modes de production industriels anciens, loin des enjeux collectifs et individuels de la transformation actuelle, sont encore très ancrés.<br>
 				</p>
